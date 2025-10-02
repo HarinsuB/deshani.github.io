e91766286f83a27f74d4f22f6c896c020e98250c
HarinsuB <harinsu89@gmail.com>
2025-10-02 22:16:32 +0530

    bugs

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/deshani-connect-clean-83/',
+  base: '/deshani.github.io/',
   server: {
     host: "::",
     port: 8080,
