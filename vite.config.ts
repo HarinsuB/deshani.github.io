c11fcf7f52eeb61153d896765caeb32cfb9b0c83
HarinsuB <harinsu89@gmail.com>
2025-10-03 08:15:05 +0530

    host

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === 'build' ? '/deshani.github.io/' : '/',
+  base: '/deshani.github.io/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}))
+})
