0fb2558966e39cea265e637253b9eb256b1e9782
HarinsuB <harinsu89@gmail.com>
2025-10-03 07:29:40 +0530

    bugs fixing

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-export default defineConfig(({ mode }) => ({
-  // IMPORTANT: set the correct base path for GitHub Pages
-  base: '/deshani.github.io/',
-
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => ({
+  plugins: [react()],
+  base: command === 'build' ? '/deshani.github.io/' : '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+}))
