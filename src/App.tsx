f2ec856d0f305217211742bde23b948ec8824ccc
HarinsuB <harinsu89@gmail.com>
2025-10-06 12:36:01 +0530

    Fix TypeScript module resolution issues by installing dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/deshani.github.io">
+      <BrowserRouter basename="/www.deshanicleaningservice.com">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
